80cdd0646e5e6b4bc8e85a1cabbfb31820a9032a
Nakiame <max.iv.slep@gmail.com>
2025-05-17 20:59:04 +0200

    Added exceptions for Config

--- src/Config.php
+++ src/Config.php
@@ -17,17 +17,57 @@ class Config
     public static function fromFile(string $filePath): self
     {
         if (!file_exists($filePath)) {
-            throw new ConfigException("Config file not found: {$filePath}");
+            throw ConfigException::fileNotFound($filePath);
         }
 
         $config = require $filePath;
+
+        if (!is_array($config)) {
+            throw ConfigException::invalidConfig('Config file must return an array');
+        }
+
         return new self($config);
     }
 
     private function validate(): void
     {
         if (empty($this->config['api']['token'])) {
-            throw new ConfigException('API token is required');
+            throw ConfigException::missingParameter('api.token');
+        }
+
+        if (empty($this->config['api']['url'])) {
+            throw ConfigException::missingParameter('api.url');
+        }
+
+        if (!is_string($this->config['api']['token'])) {
+            throw ConfigException::invalidType('api.token', 'string', $this->config['api']['token']);
+        }
+
+        if (!is_string($this->config['api']['url'])) {
+            throw ConfigException::invalidType('api.url', 'string', $this->config['api']['url']);
+        }
+
+        if (isset($this->config['api']['timeout'])) {
+            if (!is_int($this->config['api']['timeout'])) {
+                throw ConfigException::invalidType('api.timeout', 'integer', $this->config['api']['timeout']);
+            }
+            if ($this->config['api']['timeout'] <= 0) {
+                throw ConfigException::invalidValue('api.timeout', 'Timeout must be positive integer');
+            }
+        }
+
+        if ($this->config['logging']['enabled'] ?? false) {
+            if (!isset($this->config['logging']['path'])) {
+                throw ConfigException::missingParameter('logging.path');
+            }
+
+            if (!is_string($this->config['logging']['path'])) {
+                throw ConfigException::invalidType('logging.path', 'string', $this->config['logging']['path']);
+            }
+
+            if (isset($this->config['logging']['full']) && !is_bool($this->config['logging']['full'])) {
+                throw ConfigException::invalidType('logging.full', 'boolean', $this->config['logging']['full']);
+            }
         }
     }
 
@@ -54,6 +94,11 @@ class Config
     public function isLoggingFull(): bool
     {
         return $this->config['logging']['full'] ?? false;
+    }
+
+    public function isLoggingSensitiveData(): bool
+    {
+        return $this->config['logging']['sensitiveData'] ?? false;
     }
 
     public function getLogPath(): string
--- src/Exceptions/ConfigException.php
+++ src/Exceptions/ConfigException.php
@@ -2,6 +2,42 @@
 
 namespace Tinkoff\Invest\Exceptions;
 
-class ConfigException extends \RuntimeException
+class ConfigException extends ApiException
 {
+    public static function fileNotFound(string $path): self
+    {
+        return new self("Config file not found: {$path}", ['path' => $path]);
+    }
+
+    public static function invalidConfig(string $message): self
+    {
+        return new self("Invalid config: {$message}");
+    }
+
+    public static function missingParameter(string $param): self
+    {
+        return new self("Missing required config parameter: {$param}", ['parameter' => $param]);
+    }
+
+    public static function invalidType(string $param, string $expectedType, mixed $actualValue): self
+    {
+        $actualType = gettype($actualValue);
+        return new self(
+            "Invalid type for config parameter '{$param}': expected {$expectedType}, got {$actualType}",
+            [
+                'parameter' => $param,
+                'expected_type' => $expectedType,
+                'actual_type' => $actualType,
+                'actual_value' => $actualValue
+            ]
+        );
+    }
+
+    public static function invalidValue(string $param, string $message): self
+    {
+        return new self(
+            "Invalid value for config parameter '{$param}': {$message}",
+            ['parameter' => $param]
+        );
+    }
 }
